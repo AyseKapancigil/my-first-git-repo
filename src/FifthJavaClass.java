efe1cee9db596842f09549c3624849340e6f6e75
Ayse Kapancigil <Aysekapancigil@gmail.com>
2022-07-18 15:09:47 +0100

    v fifth

--- src/FifthJavaClass.java
+++ src/FifthJavaClass.java
@@ -2,4 +2,7 @@ public class FifthJavaClass {
 
     //I do not want to send to GitHub
     //this is my fifth class
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
